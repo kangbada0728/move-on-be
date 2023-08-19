c50598aeda3a5618bc75e1ab6185df54cc0e90e4
bada.kang <kangbada0728@gmail.com>
2023-08-19 21:49:04 +0900

    Initial commit

---
+++ src/main/java/me/kangbada/moveonbe/MoveOnBeApplication.java
@@ -0,0 +1,13 @@
+package me.kangbada.moveonbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoveOnBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoveOnBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/kangbada/moveonbe/MoveOnBeApplicationTests.java
@@ -0,0 +1,13 @@
+package me.kangbada.moveonbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoveOnBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

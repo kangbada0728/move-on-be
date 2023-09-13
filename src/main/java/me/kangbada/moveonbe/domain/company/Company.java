493fcdaf784b13c63ff64af7f611943071cfdc82
bada.kang <kangbada0728@gmail.com>
2023-09-13 22:02:07 +0900

    update: Company 의 기본생성자 추가

--- src/main/java/me/kangbada/moveonbe/domain/company/Company.java
+++ src/main/java/me/kangbada/moveonbe/domain/company/Company.java
@@ -5,11 +5,14 @@ import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(uniqueConstraints = {
         @UniqueConstraint(name = "NAME_UNIQUE", columnNames = {"name"})
 })
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Company {
     @EmbeddedId
     private CompanyId id;

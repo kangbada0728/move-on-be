a9ab6d1d16353e93c9e97456990e92998ab54bfe
bada.kang <kangbada0728@gmail.com>
2023-09-21 21:34:00 +0900

    add: 채용공고를 가져오는 Controller 추가

---
+++ src/main/java/me/kangbada/moveonbe/controller/CompanyJobPostController.java
@@ -0,0 +1,10 @@
+package me.kangbada.moveonbe.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class CompanyJobPostController {
+
+}

c2257ef348b818607e8e6c375befb0ec19d9bd9d
bada.kang <kangbada0728@gmail.com>
2023-10-03 21:38:23 +0900

    update: NhnCrawlingServiceTest 오류 수정

--- src/test/java/me/kangbada/moveonbe/company/nhn/NhnCrawlingServiceTest.java
+++ src/test/java/me/kangbada/moveonbe/company/nhn/NhnCrawlingServiceTest.java
@@ -10,16 +10,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class NhnCrawlingServiceTest {
 
-    public static final String 백엔드 = "백엔드";
+    public static final String 대표_채용_공고 = "[NHN PAYCO] JAVA 서버 개발";
 
     @DisplayName("NHN 기술 모집분야 채용공고 가져오기")
     @Test
     void NHN_기술_모집분야_채용공고_가져오기() {
         NhnCrawlingService nhnCrawlingService = new NhnCrawlingService();
-        List<JobPostingVo> jobPostingVos = nhnCrawlingService.crawlJobPostings();
+        List<JobPostingVo> jobPostingVos = nhnCrawlingService.crawlJobPostings(new NhnJobPostingVoBuilder());
         assertThat(jobPostingVos)
                 .flatExtracting(JobPostingVo::getTitle)
-                .contains(백엔드);
+                .containsAnyOf(대표_채용_공고);
     }
 
 }

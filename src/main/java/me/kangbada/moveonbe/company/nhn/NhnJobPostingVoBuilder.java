ceb746dc2fcd22e87536170101fd5deb2b413ae8
bada.kang <kangbada0728@gmail.com>
2023-10-03 15:51:11 +0900

    add: NHN JobPostingVoBuilder 추가

---
+++ src/main/java/me/kangbada/moveonbe/company/nhn/NhnJobPostingVoBuilder.java
@@ -0,0 +1,60 @@
+package me.kangbada.moveonbe.company.nhn;
+
+import me.kangbada.moveonbe.vo.JobPostingVoBuilder;
+import me.kangbada.moveonbe.vo.JobPostingVo;
+
+import java.net.URI;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static me.kangbada.moveonbe.domain.job.posting.JobPostingTitle.MAX_TITLE_LENGTH;
+import static org.apache.commons.lang3.StringUtils.isBlank;
+import static org.apache.commons.lang3.StringUtils.length;
+
+public class NhnJobPostingVoBuilder implements JobPostingVoBuilder {
+    private static final String YYYY_MM_DD = "yyyy.MM.dd";
+
+    private String title;
+    private LocalDate recruitPeriodStart;
+    private LocalDate recruitPeriodEnd;
+    private URI link;
+
+
+    @Override
+    public JobPostingVoBuilder title(String title) {
+        if (isBlank(title)) {
+            throw new IllegalArgumentException();
+        }
+        if (length(title) > MAX_TITLE_LENGTH) {
+            throw new IllegalArgumentException();
+        }
+        this.title = title;
+        return this;
+    }
+
+    @Override
+    public JobPostingVoBuilder recruitPeriod(String startDateStr,
+                                             String endDateStr) {
+
+        if (isBlank(startDateStr) || isBlank(endDateStr)) {
+            throw new IllegalArgumentException();
+        }
+        LocalDate startDate = LocalDate.parse(startDateStr, DateTimeFormatter.ofPattern(YYYY_MM_DD));
+        LocalDate endDate = LocalDate.parse(endDateStr);
+        this.recruitPeriodStart = startDate;
+        this.recruitPeriodEnd = endDate;
+        return this;
+    }
+
+    @Override
+    public JobPostingVoBuilder link(String link) {
+        URI uri = URI.create(link);
+        this.link = uri;
+        return this;
+    }
+
+    @Override
+    public JobPostingVo getJobPostingVo() {
+        return new JobPostingVo(title, recruitPeriodStart, recruitPeriodEnd, link);
+    }
+}

ceaee2dbfb9ce4acc358b62e3f33f74cadaeb933
bada.kang <kangbada0728@gmail.com>
2023-10-03 15:50:43 +0900

    update: JobPostController -> JobPostingController

--- src/main/java/me/kangbada/moveonbe/controller/JobPostingController.java
+++ src/main/java/me/kangbada/moveonbe/controller/JobPostingController.java
@@ -1,10 +1,12 @@
 package me.kangbada.moveonbe.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-public class JobPostController {
+@RequestMapping("/job/postings")
+public class JobPostingController {
 
 }
